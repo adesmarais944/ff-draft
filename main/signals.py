f35868a8f1e4e47adcf5c05b414cff408217f687
Adam Desmarais <98477564+adesmarais944@users.noreply.github.com>
2024-11-27 16:05:51 -0500

    added signal handler to auto-create a team when a draft is created

---
+++ main/signals.py
@@ -0,0 +1,10 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import Draft, Team
+
+@receiver(post_save, sender=Draft)
+def create_team(sender, instance, created, **kwargs):
+    if created:
+        team_name = f"User's Team for {instance.name}"
+        draft_pos = 1  # Replace with logic later
+        Team.objects.create(name=team_name, draft=instance, draft_pos=draft_pos, user=instance.user)
