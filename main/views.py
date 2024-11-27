e459784b1cb4820b4456d72349ad05a324bff6fe
Adam Desmarais <98477564+adesmarais944@users.noreply.github.com>
2024-11-27 00:34:03 -0500

    added user registration, login, and logout features

--- main/views.py
+++ main/views.py
@@ -1,6 +1,46 @@
-from django.shortcuts import render
-from django.contrib.auth import authenticate
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib import messages
+from .forms import SignUpForm
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html', {})
+
+def login_user(request):
+    print("yo")
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ('You have logged in.'))
+            return redirect('home')
+        else:
+            messages.success(request, ('There was an issue logging in. Please try again.'))
+            return redirect('login')
+    else:
+        return render(request, 'login.html', {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ('You have been logged out.'))
+    return redirect('home')
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            # Login user
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ('You have successfully registered.'))
+            return redirect('home')
+    return render(request, 'register.html', {"form":form})
