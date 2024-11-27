93c662de74e77acf95c1185c0ac1430bf0dd4b48
Adam Desmarais <98477564+adesmarais944@users.noreply.github.com>
2024-11-27 01:22:50 -0500

    registered models to admin page

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Draft, Team, Player, RosteredPlayer
 
 # Register your models here.
+admin.site.register(Draft)
+admin.site.register(Team)
+admin.site.register(Player)
+admin.site.register(RosteredPlayer)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Draft(models.Model):
+    name = models.CharField(max_length=100)
+    date = models.DateTimeField("draft created")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="draft", null=True)
+    def __str__(self):
+        return self.name
+
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+    draft = models.ForeignKey(Draft, on_delete=models.CASCADE, related_name="draft")
+    draft_pos = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="team")
+    def __str__(self):
+        return self.name
+    
+class Player(models.Model):
+    name = models.CharField(max_length=200)
+    position = models.CharField(max_length=100)
+    adp = models.DecimalField(max_digits=5, decimal_places=2)
+    def __str__(self):
+        return self.name
+    
+class RosteredPlayer(models.Model):
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    draft = models.ForeignKey(Draft, on_delete=models.CASCADE)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    pick = models.IntegerField(default=0)
+    def __str__(self):
+        return(
+            f"{self.draft} - "
+            f"{self.team} - "
+            f"{self.player} - "
+            f"pick {self.pick}"
+        )
