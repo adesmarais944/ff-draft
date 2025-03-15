e81359ad468f937ca435e21d5aee82bf4aa831e7
adam <adesmarais94@gmail.com>
2025-03-15 02:56:45 -0400

    added github actions for testing and sample test

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class BasicViewTests(TestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_about_page_status_code(self):
+        response = self.client.get('/drafts/')
+        self.assertEqual(response.status_code, 200)
